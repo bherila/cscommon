4a6742a568967669147d13c618dfeb28b39a49b9
Ben Herila <ben@herila.net>
2018-09-02 11:26:13 -0700

    Add MiscUtils http://jonskeet.uk/csharp/miscutil/

---
+++ Cohort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+	public abstract class CohortAnalysis<T>
+	{
+
+		public List<T> Data { get; set; }
+
+		public abstract string CohortGetter(T item);
+
+		public abstract decimal ValueGetter(T item);
+
+		public decimal PriorCutoffValue;
+
+	}
+
+	public class CohortRow<T>
+	{
+
+		public int N { get; set; }
+
+		public string Label { get; set; }
+
+		public List<T> Items { get; set; }
+	}
+
+}
