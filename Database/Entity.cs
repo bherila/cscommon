0837ac69ad603e7cbd131c8cf0541edb321388a5
Ben Herila <ben@herila.net>
2018-09-03 10:36:52 -0700

    Add EF Core functions

---
+++ Database/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Database
+{
+    public class Entity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
---
+++ Database/EntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.Database
+{
+    public class EntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(ent => ent.Id);
+            builder.Property(ent => ent.RowVersion).IsRowVersion();
+        }
+    }
+}
